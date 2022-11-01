2af849fd003d8b26ffc6519548b9c3a13b697cb5
Ethan Lim <ethanlimjw.21@ichat.sp.edu.sg>
2022-11-01 23:20:02 +0800

    bmi calculator

--- bmi.py
+++ bmi.py
@@ -1,18 +1,52 @@
-def calculate_bmi(height, weight):
-        print("Height = " + str(height))
-        print("Weight = " + str(weight))
+import statistics
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 
-        bmi = weight/(height*height)
 
-        print("BMI = " + str(bmi))
+def get_user_input():
+    number = input().split(",")
+    return [float(x) for x in number]
 
-        if(bmi < 18.5):
-            return("Under weight")
 
-        elif (bmi >= 18.5 and bmi <= 25.0):
-            return("Normal weight")
+def calc_average(list):
+    total = 0
+    for number in list:
+        total = total + number
+    average = total / len(list)
+    return average
 
-        else:
-            return("Overweight")
 
-print (calculate_bmi(height=1.73, weight=57))
+def find_min_max(lists):
+    min_num = min(lists)
+    max_num = max(lists)
+    return min_num, max_num
+
+
+
+
+def calc_median_temperature(middle):
+    mid = statistics.median(middle)
+    return mid
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    print(calc_average(num_list))
+    print(find_min_max(num_list))
+    print(calc_median_temperature(num_list))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+"""
+#def calc_median_temperature():
+
+def sort_temperature():
+"""
